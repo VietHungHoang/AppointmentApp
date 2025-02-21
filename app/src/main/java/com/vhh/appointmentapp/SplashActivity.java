67daf2baf3d116321734773819549ed5a5472e60
Viethung1508 <viethunghoang1508@gmail.com>
2025-02-21 16:48:27 +0700

    Add Splash activity

---
+++ app/src/main/java/com/vhh/appointmentapp/SplashActivity.java
@@ -0,0 +1,28 @@
+package com.vhh.appointmentapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.vhh.appointmentapp.databinding.ActivityMainBinding;
+import com.vhh.appointmentapp.databinding.ActivitySplashBinding;
+
+public class SplashActivity extends AppCompatActivity {
+    private ActivitySplashBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        binding = ActivitySplashBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        binding.startBtn.setOnClickListener(view -> {
+            startActivity(new Intent(SplashActivity.this, MainActivity.class));
+        });
+    }
+}
